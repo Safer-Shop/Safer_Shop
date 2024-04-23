ee5944ebcdcd4209e4f8ba1ebbab306238ed4406
saferboy <muxtordev@gmail.com>
2024-04-23 14:53:17 +0500

    connect

---
+++ src/core/middleware/index.ts
@@ -0,0 +1,5 @@
+export { default as requestLogger } from './request-logger'
+// export { permission } from './role-permission'
+// export * from './body-validate'
+// export * from './error-handler'
+// export * from './query-validate'
--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,36 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+
+import { v1 } from "@api";
+import { requestLogger } from "@core";
+// import { requestLogger, swaggerDocs } from '@core'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT ?? 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// app.use(express.static(path.join(__dirname, "../static")));
+// app.use('*.css', (req, res, next) => {
+// 	res.set('Content-Type', 'text/css')
+// 	next()
+// })
+
+app.use(requestLogger);
+
+app.get("/", (req, res) => {
+  res.redirect("/docs");
+});
+
+// app.listen(port, () => {
+//   console.log(`Server is running. Host: http://localhost:${port}`);
+//   swaggerDocs(app, +port);
+// });
+app.listen(port, () => {
+  console.log(`Server is running. Host: http://localhost:${port}`);
+});
